1a0328b836683a138cce8b510e59ba654f115c80
Brock Hutchings <brockahutchings@gmail.com>
2017-04-06 09:45:23 -0400

    add template for FTNs

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The folloeing two ftns are used to retrieve the inverse of a matrix 
+##and store it in a cach so theat the next time a user tries to query 
+##it they will get the saved value
 
-## Write a short comment describing this function
-
+## creates a cache and has function to
+## get/set the matrix
+## get/set the cache
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  ##define hte cache to be used
+  c <- NULL
+  
+  ##setter ftn() for the matrix
+  set <- function(){
+    
+  }
+  
+  #getter ftn() for the matrix 
+  get <- function(){
+    
+  }
+  
+  ##set the cache c to the inverse of the matrix
+  set_inverse <- function(){
+    
+  }
+  
+  ##get the cache c
+  get_inverse <- function(){
+    
+  }
 }
 
-
-## Write a short comment describing this function
-
+##inverts the matrix returned by makeCacheMatrix
+##or if matrix has already been inverted retrieve it from cache
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
 }
